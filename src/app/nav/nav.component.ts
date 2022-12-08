10af63b301129fae8eaf570988688ccb0d509cdf
Phạm Tiến Dũng <56197293+phamtiendungcw@users.noreply.github.com>
2022-12-08 17:42:18 +0700

    Closes #1

---
+++ src/app/nav/nav.component.ts
@@ -0,0 +1,32 @@
+/*
+ * Copyright (c) 2022. DungCW
+ */
+
+import { Component, OnInit } from '@angular/core';
+import { AccountService } from "../_services/account.service";
+
+@Component({
+  selector: 'app-nav',
+  templateUrl: './nav.component.html',
+  styleUrls: ['./nav.component.css'],
+})
+export class NavComponent implements OnInit {
+  model: any = {};
+
+  constructor(public accountService: AccountService) {}
+
+  ngOnInit(): void {}
+
+  login() {
+    this.accountService.login(this.model).subscribe({
+      next: (res) => {
+        console.log(res);
+      },
+      error: (err) => console.log(err)
+    })
+  }
+
+  logout() {
+    this.accountService.logout();
+  }
+}
